53a502891db27a47b94bb88a7be135901ece64a5
Budi77Darmawan <budymitman_77@yahoo.co.id>
2021-12-05 15:41:50 +0700

    Initial Commit

---
+++ CommonExt/CommonExt.h
@@ -0,0 +1,18 @@
+//
+//  CommonExt.h
+//  CommonExt
+//
+//  Created by Budi Darmawan on 05/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonExt.
+FOUNDATION_EXPORT double CommonExtVersionNumber;
+
+//! Project version string for CommonExt.
+FOUNDATION_EXPORT const unsigned char CommonExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonExt/PublicHeader.h>
+
+
